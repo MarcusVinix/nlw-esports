640f1c3af6f1aa489796282f00b756147c0f8f16
MarcusVinix <vi.ni159marcus@gmail.com>
2022-09-16 09:47:59 -0300

    creating backend

--- server/build/server.js
+++ server/build/server.js
@@ -1,15 +1,38 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const client_1 = require("@prisma/client");
 const app = (0, express_1.default)();
-app.get('/ads', (request, response) => {
+const prisma = new client_1.PrismaClient();
+app.get('/games', (request, response) => __awaiter(void 0, void 0, void 0, function* () {
+    const games = yield prisma.game.findMany();
+    return response.status(200).json(games);
+}));
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+});
+app.get('/games/:id/ads', (request, response) => {
+    // const adId = request.params.id;
     return response.json([
         { id: 1, name: 'Anúncio 1' },
         { id: 2, name: 'Anúncio 2' },
         { id: 3, name: 'Anúncio 3' },
     ]);
+});
+app.get('/ads/:id/discord', (request, response) => {
+    // const adId = request.params.id;
+    return response.json([]);
 });
 app.listen(3333);
